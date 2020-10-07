8091f8523bda63bd1b09f5d4ceab1d8b48bff80e
Gleb <klenka2017@gmail.com>
2020-10-07 21:04:13 +0300

    Added botton control (homework #2)

--- lesson_02_home_button_control/lesson_02_home_button_control.ino
+++ lesson_02_home_button_control/lesson_02_home_button_control.ino
@@ -1,12 +1,22 @@
 void setup() {
   pinMode(3, INPUT);
   digitalWrite(3, HIGH);
+  pinMode(4, INPUT);
+  digitalWrite(4, HIGH);
   Serial.begin(9600);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-  int button_state = digitalRead(3);
-  if (button_state == 1)
-    Serial.print('1');
+  int button_state_3 = digitalRead(3);
+  int button_state_4 = digitalRead(4);
+  if (button_state_3 == 0)
+  {
+    digitalWrite(9,HIGH);
+  }
+  if (button_state_4 == 0)
+  {
+    digitalWrite(9,LOW);
+  }
 
 }
