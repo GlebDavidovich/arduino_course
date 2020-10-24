4137dc405bd331fab7f915553cc9ac827b407da2
Gleb <klenka2017@gmail.com>
2020-10-24 09:53:52 +0300

    Line control added

---
+++ lesson_04_line_sensor/lesson_04_line_sensor.ino
@@ -0,0 +1,17 @@
+#define LINE_PIN A2
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int data = analogRead(LINE_PIN);
+  if (data > 50)
+  {
+    Serial.println("White ");
+  } else
+  {
+    Serial.println("Black ");
+  }
+
+}
